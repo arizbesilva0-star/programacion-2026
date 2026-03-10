c0e7757668f6498fd4be2c27c188bbbf910e3662
arizbesilva0-star <arizbesilva0@gmail.com>
2026-03-10 09:29:03 -0600

    Add files via upload

--- teoria/operadorPunto/Cuenta.py
+++ teoria/operadorPunto/Cuenta.py
@@ -1,23 +1,27 @@
-'''
-Created on Feb, 2019
+"""
+Created on Febrero, 2019
 @author: arizbesilva0-star
+"""
 
-'''
-class Cuenta :
-  #este es el metodo constructor, que
-  #inicializa a los atributos de una clase
-  # La clase ya tiene más de un atributo.
-	# En este caso, cambia la cantidad de argumentos que el
-	# constructor recibe
-  def __init__(self, valor1, valor2, valor3) :
-    self.saldo=valor1
-    self.tipo=valor2
-    self.fechaCreacion=valor3
-  def depositar(self, cantidad) :
-    self.saldo=self.saldo+cantidad
-  def retirar(self, cantidad2) :
-    self.saldo=self.saldo-cantidad2
-  def informacion(self):
-    print("saldo::",self.saldo)
-    print("tipo::",self.tipo)
-    print("fechaCreacion::",self.fechaCreacion)
+class Cuenta:
+
+    # Método constructor
+    # Inicializa los atributos de la cuenta
+    def __init__(self, saldo, tipo, fechaCreacion):
+        self.saldo = saldo
+        self.tipo = tipo
+        self.fechaCreacion = fechaCreacion
+
+    # Método para depositar dinero
+    def depositar(self, cantidad):
+        self.saldo = self.saldo + cantidad
+
+    # Método para retirar dinero
+    def retirar(self, cantidad):
+        self.saldo = self.saldo - cantidad
+
+    # Método para mostrar la información de la cuenta
+    def imprimirDetalles(self):
+        print("saldo:", self.saldo)
+        print("tipo:", self.tipo)
+        print("fechaCreacion:", self.fechaCreacion)
