62a48061d61fc4fed0519c362c73b4fae3d406ef
arizbesilva0-star <arizbesilva0@gmail.com>
2026-04-27 11:56:44 -0600

    Update Cuenta.py

--- teoria/objetosComoCadenas/Cuenta.py
+++ teoria/objetosComoCadenas/Cuenta.py
@@ -6,12 +6,18 @@ Created on Marzo, 2026
 class Cuenta:
 
     def __init__(self, saldo, tipo):
-        self.saldo = saldo
-        self.tipo = tipo
+        self.__saldo = saldo
+        self.__tipo = tipo
 
     def depositar(self, cantidad):
         if cantidad > 0:
-            self.saldo += cantidad
+            self.__saldo += cantidad
+
+    def getSaldo(self):
+        return self.__saldo
+
+    def getTipo(self):
+        return self.__tipo
 
     def __str__(self):
-        return "Saldo: " + str(self.saldo) + ", Tipo: " + self.tipo
+        return "Saldo: " + str(self.__saldo) + ", Tipo: " + self.__tipo
