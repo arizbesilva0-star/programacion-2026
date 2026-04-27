680ca4218aa025884c4bec4e115e557bf15511ff
arizbesilva0-star <arizbesilva0@gmail.com>
2026-04-27 12:02:00 -0600

    Update Main.py

--- teoria/objetosComoCadenas/Main.py
+++ teoria/objetosComoCadenas/Main.py
@@ -9,7 +9,10 @@ from cliente import *
 print("Desde las pruebas")
 
 cuenta1 = Cuenta(300, "debito")
+print(cuenta1)
+
 cuenta1.depositar(400)
+print(cuenta1)
 
 cliente1 = Cliente("Virginia", "direccion", 25, cuenta1)
-cliente1.mostrarDetalles()
+print(cliente1)
