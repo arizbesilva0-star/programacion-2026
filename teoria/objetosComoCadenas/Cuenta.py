fc51d37cf96e75fa62127195077d460bd9623f49
arizbesilva0-star <arizbesilva0@gmail.com>
2026-04-27 11:41:33 -0600

    Update Cuenta.py

--- teoria/objetosComoCadenas/Cuenta.py
+++ teoria/objetosComoCadenas/Cuenta.py
@@ -5,33 +5,13 @@ Created on Marzo, 2026
 
 class Cuenta:
 
-    def __init__(self, saldo, tipo, fechaCreacion):
-        # ATRIBUTOS PRIVADOS (no se pueden acceder directamente desde fuera)
-        self.__saldo = saldo
-        self.__tipo = tipo
-        self.__fechaCreacion = fechaCreacion
+    def __init__(self, saldo, tipo):
+        self.saldo = saldo
+        self.tipo = tipo
 
     def depositar(self, cantidad):
-        # MÉTODO PÚBLICO
-        if cantidad <= 0:
-            return False
-        self.__saldo += cantidad
-        return True
-
-    def retirar(self, cantidad):
-        # MÉTODO PÚBLICO
-        if cantidad > self.__saldo:
-            return False
-        self.__saldo -= cantidad
-        return True
-
-    # GETTERS (métodos públicos para acceder a atributos privados)
-    def getSaldo(self):
-        return self.__saldo
-
-    def getTipo(self):
-        return self.__tipo
+        if cantidad > 0:
+            self.saldo += cantidad
 
     def __str__(self):
-        # MÉTODO PÚBLICO
-        return "Saldo: " + str(self.__saldo) + ", Tipo: " + self.__tipo
+        return "Saldo: " + str(self.saldo) + ", Tipo: " + self.tipo
