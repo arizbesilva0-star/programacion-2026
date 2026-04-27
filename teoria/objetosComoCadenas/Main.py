4401f2067259b4d7dd28b1a1729c70657f21af77
arizbesilva0-star <arizbesilva0@gmail.com>
2026-04-27 11:57:29 -0600

    Update Main.py

--- teoria/objetosComoCadenas/Main.py
+++ teoria/objetosComoCadenas/Main.py
@@ -3,40 +3,13 @@ Created on Marzo, 2026
 @author: arizbesilva0-star
 """
 
-from Cuenta import *
-from Cliente import *
-from Menu import *
+from cuenta import *
+from cliente import *
 
-menu = Menu("Bienvenido al Banco")
+print("Desde las pruebas")
 
-cuenta1 = Cuenta(300, "debito", "12/02/2019")
-cliente1 = Cliente("Arizbe", cuenta1)
+cuenta1 = Cuenta(300, "debito")
+cuenta1.depositar(400)
 
-menu.bienvenida()
-
-while True:
-
-    op = menu.opciones()
-
-    if op == "1":
-        cant = float(input("Cantidad: "))
-        if cuenta1.depositar(cant):
-            print("Deposito correcto")
-        else:
-            print("Error")
-
-    elif op == "2":
-        cant = float(input("Cantidad: "))
-        if cuenta1.retirar(cant):
-            print("Retiro correcto")
-        else:
-            print("No hay saldo")
-
-    elif op == "3":
-        print(cliente1)
-
-    elif op == "4":
-        break
-
-    else:
-        print("Opcion invalida")
+cliente1 = Cliente("Virginia", "direccion", 25, cuenta1)
+cliente1.mostrarDetalles()
