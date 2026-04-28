69e0c417e38dfd4298330a254812a58f800175e5
arizbesilva0-star <arizbesilva0@gmail.com>
2026-04-27 23:51:34 -0600

    Update Cuenta.py

--- teoria/metodosVSMain/Cuenta.py
+++ teoria/metodosVSMain/Cuenta.py
@@ -5,22 +5,14 @@ Created on Marzo, 2026
 
 class Cuenta:
 
-    def __init__(self, saldo, tipo, fechaCreacion):
-        self.saldo = saldo
+    def __init__(self, cantidad, tipo):
+        self.cantidad = cantidad
         self.tipo = tipo
-        self.fechaCreacion = fechaCreacion
 
-    def depositar(self, cantidad):
-        if cantidad <= 0:
-            return False
-        self.saldo += cantidad
-        return True
+    def depositar(self, valor):
+        if valor > 0:
+            self.cantidad = self.cantidad + valor
 
-    def retirar(self, cantidad):
-        if cantidad > self.saldo:
-            return False
-        self.saldo -= cantidad
-        return True
-
-    def __str__(self):
-        return "Saldo: " + str(self.saldo) + ", Tipo: " + self.tipo
+    def mostrarDetalles(self):
+        print("Cantidad::", self.cantidad)
+        print("Tipo::", self.tipo)
