9367f6294fa3bd2261d3a0451ae7f9d969d7b317
arizbesilva0-star <arizbesilva0@gmail.com>
2026-04-27 23:52:07 -0600

    Update Main.py

--- teoria/metodosVSMain/Main.py
+++ teoria/metodosVSMain/Main.py
@@ -3,40 +3,19 @@ Created on Marzo, 2026
 @author: arizbesilva0-star
 """
 
-from Cuenta import *
-from Cliente import *
-from Menu import *
+from cuenta import *
 
-menu = Menu("Bienvenido al Banco")
+class Main:
+    pass
 
-cuenta1 = Cuenta(300, "debito", "12/02/2019")
-cliente1 = Cliente("Arizbe", cuenta1)
 
-menu.bienvenida()
+print("Desde las pruebas")
 
-while True:
+cuenta1 = Cuenta(300, "debito")
 
-    op = menu.opciones()
+print("Cantidad inicial::", cuenta1.cantidad)
+print("Tipo de cuenta::", cuenta1.tipo)
 
-    if op == "1":
-        cant = float(input("Cantidad: "))
-        if cuenta1.depositar(cant):
-            print("Deposito correcto")
-        else:
-            print("Error")
+cuenta1.depositar(200)
 
-    elif op == "2":
-        cant = float(input("Cantidad: "))
-        if cuenta1.retirar(cant):
-            print("Retiro correcto")
-        else:
-            print("No hay saldo")
-
-    elif op == "3":
-        print(cliente1)
-
-    elif op == "4":
-        break
-
-    else:
-        print("Opcion invalida")
+cuenta1.mostrarDetalles()
