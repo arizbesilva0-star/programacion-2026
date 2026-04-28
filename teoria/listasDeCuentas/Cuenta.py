81544191dccc377518269b79f35b296e1016e880
arizbesilva0-star <arizbesilva0@gmail.com>
2026-04-28 00:18:51 -0600

    Update Cuenta.py

--- teoria/listasDeCuentas/Cuenta.py
+++ teoria/listasDeCuentas/Cuenta.py
@@ -1,31 +1,27 @@
 """
-Created on Marzo, 2026
+Created on Abril, 2026
 @author: arizbesilva0-star
 """
 
 class Cuenta:
 
     def __init__(self, saldo, tipo, fechaCreacion):
-        # ATRIBUTOS PRIVADOS (no se pueden acceder directamente desde fuera)
         self.__saldo = saldo
         self.__tipo = tipo
         self.__fechaCreacion = fechaCreacion
 
     def depositar(self, cantidad):
-        # MÉTODO PÚBLICO
         if cantidad <= 0:
             return False
         self.__saldo += cantidad
         return True
 
     def retirar(self, cantidad):
-        # MÉTODO PÚBLICO
-        if cantidad > self.__saldo:
+        if cantidad > self.__saldo or cantidad <= 0:
             return False
         self.__saldo -= cantidad
         return True
 
-    # GETTERS (métodos públicos para acceder a atributos privados)
     def getSaldo(self):
         return self.__saldo
 
@@ -33,5 +29,4 @@ class Cuenta:
         return self.__tipo
 
     def __str__(self):
-        # MÉTODO PÚBLICO
         return "Saldo: " + str(self.__saldo) + ", Tipo: " + self.__tipo
