06dc73b0502977982759371ef182757ed0b3ce4d
arizbesilva0-star <arizbesilva0@gmail.com>
2026-04-28 00:18:15 -0600

    Update Cliente.py

--- teoria/listasDeCuentas/Cliente.py
+++ teoria/listasDeCuentas/Cliente.py
@@ -1,15 +1,36 @@
 """
-Created on Marzo, 2026
+Created on Abril, 2026
 @author: arizbesilva0-star
 """
 
 class Cliente:
 
-    def __init__(self, nombre, cuenta):
-        # ATRIBUTOS PÚBLICOS
+    def __init__(self, nombre):
         self.nombre = nombre
-        self.cuenta = cuenta
+        self.cuentas = []
+
+    def agregarCuenta(self, cuenta):
+        self.cuentas.append(cuenta)
+
+    def eliminarCuenta(self, indice):
+        if 0 <= indice < len(self.cuentas):
+            self.cuentas.pop(indice)
+        else:
+            print("Indice invalido")
+
+    def recuperarCuenta(self, indice):
+        if 0 <= indice < len(self.cuentas):
+            return self.cuentas[indice]
+        else:
+            print("Indice invalido")
+            return None
+
+    def mostrarCuentas(self):
+        if len(self.cuentas) == 0:
+            print("No hay cuentas registradas")
+        else:
+            for i, cta in enumerate(self.cuentas):
+                print(i, "-", cta)
 
     def __str__(self):
-        # MÉTODO PÚBLICO (usa getters porque Cuenta es privada internamente)
-        return "Cliente: " + self.nombre + " | " + self.cuenta.getTipo() + " | Saldo: " + str(self.cuenta.getSaldo())
+        return "Cliente: " + self.nombre + " | Total cuentas: " + str(len(self.cuentas))
