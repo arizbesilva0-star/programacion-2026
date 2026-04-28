c7f2fbf699d92646dad694b1e1503211b1675695
arizbesilva0-star <arizbesilva0@gmail.com>
2026-04-27 23:38:48 -0600

    Update Main.py

--- teoria/relacionesEntreObjetos/Main.py
+++ teoria/relacionesEntreObjetos/Main.py
@@ -3,40 +3,16 @@ Created on Marzo, 2026
 @author: arizbesilva0-star
 """
 
-from Cuenta import *
-from Cliente import *
-from Menu import *
+from cuenta import *
+from cliente import *
 
-menu = Menu("Bienvenido al Banco")
+print("Desde las pruebas")
 
-cuenta1 = Cuenta(300, "debito", "12/02/2019")
-cliente1 = Cliente("Arizbe", cuenta1)
+cuenta1 = Cuenta(300, "debito")
+cuenta1.mostrarDetalles()
 
-menu.bienvenida()
+cuenta1.depositar(400)
+cuenta1.mostrarDetalles()
 
-while True:
-
-    op = menu.opciones()
-
-    if op == "1":
-        cant = float(input("Cantidad: "))
-        if cuenta1.depositar(cant):
-            print("Deposito correcto")
-        else:
-            print("Error")
-
-    elif op == "2":
-        cant = float(input("Cantidad: "))
-        if cuenta1.retirar(cant):
-            print("Retiro correcto")
-        else:
-            print("No hay saldo")
-
-    elif op == "3":
-        print(cliente1)
-
-    elif op == "4":
-        break
-
-    else:
-        print("Opcion invalida")
+cliente1 = Cliente("Arizbe", "direccion", 25, cuenta1)
+cliente1.mostrarDetalles()
