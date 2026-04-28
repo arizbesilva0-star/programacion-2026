b0826c6f2b35a4034db52e9725c3c79c1b13e06b
arizbesilva0-star <arizbesilva0@gmail.com>
2026-04-27 23:37:33 -0600

    Update Cliente.py

--- teoria/relacionesEntreObjetos/Cliente.py
+++ teoria/relacionesEntreObjetos/Cliente.py
@@ -5,11 +5,14 @@ Created on Marzo, 2026
 
 class Cliente:
 
-    def __init__(self, nombre, cuenta):
-        # ATRIBUTOS PÚBLICOS
+    def __init__(self, nombre, direccion, edad, cuenta):
         self.nombre = nombre
+        self.direccion = direccion
+        self.edad = edad
         self.cuenta = cuenta
 
-    def __str__(self):
-        # MÉTODO PÚBLICO (usa getters porque Cuenta es privada internamente)
-        return "Cliente: " + self.nombre + " | " + self.cuenta.getTipo() + " | Saldo: " + str(self.cuenta.getSaldo())
+    def mostrarDetalles(self):
+        print("Nombre::", self.nombre)
+        print("Direccion::", self.direccion)
+        print("Edad::", self.edad)
+        self.cuenta.mostrarDetalles()
