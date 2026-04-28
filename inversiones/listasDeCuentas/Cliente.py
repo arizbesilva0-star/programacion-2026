cb4a184a2b0dc8aded981dfabcc6ad2ef6721242
arizbesilva0-star <arizbesilva0@gmail.com>
2026-04-28 11:06:23 -0600

    Update Cliente.py

--- inversiones/listasDeCuentas/Cliente.py
+++ inversiones/listasDeCuentas/Cliente.py
@@ -1,5 +1,5 @@
 """
-Created on March, 2026
+Created on April, 2026
 @author: arizbesilva-star
 """
 
@@ -9,6 +9,34 @@ class Cliente:
         self.__nombre = nombre
         self.__direccion = direccion
         self.__edad = edad
+        self.__cuentas = []  # lista de cuentas (Inversion)
+
+    def agregarCuenta(self, cuenta):
+        self.__cuentas.append(cuenta)
+
+    def borrarCuenta(self, nombre):
+        for cuenta in self.__cuentas:
+            if cuenta.mostrar()[0] == nombre:
+                self.__cuentas.remove(cuenta)
+                return True
+        return False
+
+    def recuperarCuenta(self, nombre):
+        for cuenta in self.__cuentas:
+            if cuenta.mostrar()[0] == nombre:
+                return cuenta
+        return None
+
+    def mostrarCuentas(self):
+        if len(self.__cuentas) == 0:
+            print("No hay cuentas registradas")
+        else:
+            for cuenta in self.__cuentas:
+                nombre, saldo, interes, tiempo = cuenta.mostrar()
+                print("\nNombre:", nombre)
+                print("Saldo:", saldo)
+                print("Interes:", interes)
+                print("Tiempo:", tiempo, "meses")
 
     def __str__(self):
         texto = ""
